d23694731ec5979e0a8c0a887e1458c0d32c0c12
Roger Lipscombe <roger@differentpla.net>
2019-11-15 19:59:33 +0000

    Initial creation

---
+++ lib/mix_eunit.ex
@@ -0,0 +1,18 @@
+defmodule MixEunit do
+  @moduledoc """
+  Documentation for MixEunit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixEunit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
