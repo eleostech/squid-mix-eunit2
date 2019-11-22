7710866e22705ec3d239428246e91c6fec246718
Roger Lipscombe <roger@differentpla.net>
2019-11-22 18:11:22 +0000

    Merge eunit and eunit.test tasks
    When we use apps_paths, we get the list of apps in an arbitrary order,
    which means that we can't reliably compile dependent apps in the correct
    order.
    
    This means that -include_lib doesn't work reliably.
    
    So: move the responsibility for compiling stuff in the correct order
    elsewhere.
    
    That is:
    
        MIX_ENV=test mix do compile, eunit
    
    So 'compile' compiles everything in the right order, and 'eunit' _only_
    runs the tests.

--- lib/mix_eunit.ex
+++ lib/mix_eunit.ex
@@ -1,30 +1,46 @@
 defmodule Mix.Tasks.Eunit do
   use Mix.Task
 
-  # The main task should NOT be recursive; we want to explicitly enumerate
-  # child projects, rather than allowing mix to do it.
-  # Specifically, if we attempt to run "mix compile" in a child project,
-  # mix compiles everything, _before_ we have a chance to modify the
-  # compiler options
-  @recursive false
+  @recursive true
   @preferred_cli_env :test
+
+  def run, do: run([])
 
   @impl true
   def run(_args) do
-    # We need to load dependencies first.
-    Mix.Task.run("loadpaths")
+    opts = [:verbose]
 
-    # Iterate over child projects.
-    for {app, path} <- Mix.Project.apps_paths || [] do
-      post_config = [
-        erlc_paths: ["src", "test"],
-        erlc_options: [{:d, :TEST}, {:debug_info}]
-      ]
-      Mix.Project.in_project(app, path, post_config, fn _ ->
-        Mix.Task.run("compile")
-      end)
-    end
+    # ".../top/_build/test/lib/app"
+    app_path = Mix.Project.app_path()
+    ebin_path = Path.join([app_path, "ebin"])
 
-    Mix.Task.run("eunit.test")
+    # Ensure that 'ebin' is in the code search path; if the app isn't mentioned
+    # in a dependency, it doesn't get added by default.
+    Code.append_path(ebin_path)
+
+    modules = get_test_modules(ebin_path)
+    :eunit.test(modules, opts)
+  end
+
+  defp get_test_modules(ebin_path) do
+    glob = Path.join([ebin_path, "*.beam"])
+
+    Path.wildcard(glob)
+    |> Enum.map(&(Path.basename(&1, ".beam")))
+    |> remove_duplicates
+    |> Enum.map(&String.to_atom/1)
+  end
+
+  defp remove_duplicates(modules) do
+    # If 'module' has a corresponding 'modules_tests', remove the '_tests' variant.
+    List.foldl(modules, modules, fn m, acc ->
+      m_tests = m <> "_tests"
+
+      if Enum.member?(acc, m_tests) do
+        List.delete(acc, m_tests)
+      else
+        acc
+      end
+    end)
   end
 end
