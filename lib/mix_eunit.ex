b73a23632249398e0565ee47c0fe97778a04de6c
Roger Lipscombe <roger@differentpla.net>
2022-01-05 20:48:21 +0000

    Add "--module" option

--- lib/mix_eunit.ex
+++ lib/mix_eunit.ex
@@ -18,6 +18,7 @@ defmodule Mix.Tasks.Eunit do
     * `--verbose` - enables verbose output
     * `--surefire` - enables Surefire-compatible XML output
     * `--cover` - exports coverage data (as `eunit.coverdata`)
+    * `--module MODULE` - runs tests from MODULE, rather than all tests, can be specified multiple times.
 
   ## Coverage
 
@@ -34,7 +35,9 @@ defmodule Mix.Tasks.Eunit do
   @impl true
   def run(args) do
     {opts, _, _} =
-      OptionParser.parse(args, strict: [verbose: :boolean, surefire: :boolean, cover: :boolean])
+      OptionParser.parse(args,
+        strict: [verbose: :boolean, surefire: :boolean, cover: :boolean, module: [:string, :keep]]
+      )
 
     Mix.shell().print_app()
     project = Mix.Project.config()
@@ -49,7 +52,7 @@ defmodule Mix.Tasks.Eunit do
     # in a dependency, it doesn't get added by default.
     Code.append_path(ebin_path)
 
-    modules = get_test_modules(ebin_path)
+    modules = get_test_modules(opts, ebin_path)
     eunit_opts = convert_opts(opts, app_path)
 
     if opts[:cover] do
@@ -98,6 +101,19 @@ defmodule Mix.Tasks.Eunit do
   defp convert_opt({:surefire, true}, app_path), do: [{:report, {:eunit_surefire, [{:dir, app_path}]}}]
   defp convert_opt(_, _), do: []
 
+  defp get_test_modules(opts, ebin_path) do
+    modules = for {:module, m} <- opts, do: String.to_atom(m)
+    app_modules = get_test_modules(ebin_path)
+
+    # If the user has specified any modules, we only want to run those that occur in the current app.
+    case modules do
+      [] -> app_modules
+      modules -> Enum.filter(modules, &Enum.member?(app_modules, &1))
+    end
+  end
+
+  # Get the list of .beam files in the given 'ebin' directory, and convert them to module names.
+  # If there's a 'foo' and a 'foo_test', discard the 'foo' variant; Erlang will convert 'foo' to 'foo_test' later.
   defp get_test_modules(ebin_path) do
     glob = Path.join([ebin_path, "*.beam"])
 
